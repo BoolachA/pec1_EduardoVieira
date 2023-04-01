b5ccfbccdcebc22db153e1cdb9e65082937ea829
Eduardo Vieira <bolacha0953@gmail.com>
2023-04-01 03:36:57 +0200

    Creacion metodo crearAlumnos

--- src/edu/ucj/programacion/pec1/eduardoVieira/UtilsColegio.java
+++ src/edu/ucj/programacion/pec1/eduardoVieira/UtilsColegio.java
@@ -1,8 +1,36 @@
 package edu.ucj.programacion.pec1.eduardoVieira;
 
+import java.util.Scanner;
+
 public class UtilsColegio {
 	public static Alumno[] crearAlumnos(int numAlumnos) {
 		Alumno[] alumnos = new Alumno[numAlumnos];
+		
+		String[] nombres = new String[numAlumnos];
+		String[] apellidos = new String[numAlumnos];
+		String[] dnis = new String[numAlumnos];
+		
+		Scanner scanner = new Scanner(System.in);
+		for(int x=0;x<numAlumnos;x++) {
+			System.out.println("Nombre del alumno " + (x+1) + ": ");
+			nombres[x] = scanner.nextLine();
+		}
+		for(int x=0;x<numAlumnos;x++) {
+			System.out.println("Apellidos del alumno " + (x+1) + ": ");
+			apellidos[x] = scanner.nextLine();
+		}
+		for(int x=0;x<numAlumnos;x++) {
+			System.out.println("DNI del alumno " + (x+1) + ": ");
+			dnis[x] = scanner.nextLine();
+		}
+		scanner.close();
+		
+		for(int x=0;x<numAlumnos;x++) {
+			alumnos[x] = new Alumno(nombres[x],apellidos[x],dnis[x]);
+		}
+		
+		//Prueba
+		alumnos[1].getNombre();
 		
 		return alumnos;
 	}
